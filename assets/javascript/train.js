72822dc8b743e6d26637a6d1e157a5e73af96184
Pavani Vithala <pavani.vithala18@gmail.com>
2018-11-05 22:37:03 -0500

    Added clock logic

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -1,18 +1,21 @@
-$(document).ready(function () {
-    var currentTime = moment();
-    var timeDisplay = moment(currentTime).format("hh:mm");
-    console.log("The Current time is "+timeDisplay);
-   // $("#train-name").setFocus();
-    $(".currentTime").text(timeDisplay);
 
- $('#submit').on('click', function () {
-    var trainName = $("#train-name").text().trim();
+$(document).ready(function () {
+    var update = function() {
+     $("#currentTime").text(moment().format('hh:mm:ss'));
+    }
+     setInterval(update, 1000);
+     $("#train-name").focus();
+   
+ $("#Submit").on("click", function (event) {
+    event.preventDefault();
+     console.log("Entered on click function");
+    var trainName = $("#train-name").val().trim();
     console.log("Train name is " + trainName);
-    var destination = $("#train-destination").text().trim();
+    var destination = $("#train-destination").val().trim();
     console.log("Train Destination is " + destination);
-    var firstTime = $("#train-departure").text().trim();
+    var firstTime = $("#train-departure").val().trim();
     console.log("Train first time is " + firstTime);
-    var frequency = $("#train-frequency").text().trim();
+    var frequency = $("#train-frequency").val().trim();
     console.log("Train frequency is " + frequency); 
 
 
